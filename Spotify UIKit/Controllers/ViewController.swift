7b3bfd09570b4ba8b3ed775ad1376a8c8d89e14e
irangareddy <itsmerr17@gmail.com>
2021-08-18 15:07:13 +0530

    Initial Commit

---
+++ Spotify UIKit/Controllers/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Spotify UIKit
+//
+//  Created by RANGA REDDY NUKALA on 18/08/21.
+//
+
+import UIKit
+
+/// UIViewController: A view controller’s main responsibilities include the following:
+/// ✅    Updating the contents of the views, usually in response to changes to the underlying data.
+/// ✅    Responding to user interactions with views.
+/// ✅    Resizing views and managing the layout of the overall interface.
+/// ✅    Coordinating with other objects—including other view controllers—in your app.
+///
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .systemRed
+    }
+}
+
